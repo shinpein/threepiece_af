ed61c08e71a1a03beac2bcedabac5363dc2ff2b4
shinpein <development.proceed@gmail.com>
2021-01-27 10:59:15 +0900

    new add

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Access Analysis',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      },
+    ],
+    link: [{
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css'
+      },
+
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/assets/css/main.css',
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [{
+    src: '~/plugins/moment-filter',
+    ssr: false
+  }, ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    ['@nuxtjs/moment', ['ja']],
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: 'https://'
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {}
+}
